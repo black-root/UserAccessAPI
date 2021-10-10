907406afa997369582bd162d2b62644852ecbbd6
Ronald Ibañez <ronald.ibanez1994@gmail.com>
2021-10-09 21:19:02 -0600

    Started Project

---
+++ src/main/java/com/blackroot/useraccess/UseraccessApplication.java
@@ -0,0 +1,13 @@
+package com.blackroot.useraccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseraccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseraccessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blackroot/useraccess/UseraccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackroot.useraccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseraccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
